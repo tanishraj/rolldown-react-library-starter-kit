837c82204acc15fbdb9270e9680edf914314e709
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-09 15:45:56 +0400

    feat: external configuration for rolldown

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from "rolldown";
+import packageJson from "./package.json";
 
 export default defineConfig([
   {
     input: "src/index.ts",
     output: [
       {
-        dir: "dist",
+        file: packageJson.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
         format: "esm",
+        exports: "named",
+        sourcemap: true,
       },
     ],
+    external: ["react", "react-dom", "react/jsx-runtime"],
   },
 ]);
