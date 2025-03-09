fbdb93dd4ad07a43327e76fd64fd7f2b5a8829bc
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-09 14:26:12 +0400

    feat: updated button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
 
-export const Button = () => {
-  return <button>Button</button>;
+export type ButtonProps = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+};
+
+export const Button: FC<ButtonProps> = ({ children, variant }) => {
+  return (
+    <button style={{ backgroundColor: variant === "primary" ? "red" : "blue" }}>
+      {children}
+    </button>
+  );
 };
